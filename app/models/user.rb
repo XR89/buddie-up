696ec74238e4ea2f87caa298e940ccd6c1d6dffd
Louis Drinkwater <izumilouis95@gmail.com>
2021-08-31 17:50:30 +0100

    Add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,76 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, :region, :dob, :gender, :language, presence: true
+  validates :username, format: { with: /\A[A-Za-z0-9\-_.]+\z/i,
+                                 message: "Please use only letters from the latin alphabet,
+                                           numbers, and punctuation: -_." },
+                       length: { minimum: 3, maximum: 255 },
+                       uniqueness: true
+  validates :region, inclusion: { in: @regions,
+                                  message: "%<value> is not a valid region" }
+
+  # validation for languages included in variable below
+  validate :validate_age
+  validate :gender, inclusion: { in: ["Male", "Female", "Non-binary", "Prefer not to say"] }
+
+  # validation for favourite users
+  # validation for avoid users
+  # validation for user status (0 - offline, 1 - online)?
+
+  # update when schema finalised
+  has_many :user_ratings
   has_one_attached :photo
+
+  private
+
+  def validate_age
+    if dob.present? && dob > 18.years.ago.to_d
+      errors.add(:dob, 'You must be at least 18 years old.')
+    end
+  end
+
+  @regions = [
+    "Europe",
+    "North America",
+    "South America",
+    "Africa",
+    "Middle East",
+    "East Asia",
+    "Southeast Asia",
+    "Australia & Pacific",
+    "Antarctica"
+  ]
+
+  @languages = %w[
+    English
+    Czech
+    Danish
+    Dutch
+    Finnish
+    French
+    German
+    Hungarian
+    Italian
+    Japanese
+    Korean
+    Norwegian
+    Polish
+    Portuguese
+    Romanian
+    Russian
+    Simplified Chinese
+    Spanish
+    Swedish
+    Thai
+    Traditional Chinese
+    Turkish
+    Bulgarian
+    Ukrainian
+    Greek
+    Vietnamese
+    Arabic
+    Hindi
+    Other
+  ]
 end
