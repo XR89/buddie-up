9263cec575c543e22b5554aa4b71c32884cfe000
Louis Drinkwater <izumilouis95@gmail.com>
2021-08-31 19:05:02 +0100

    Seed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,33 @@
+require 'bigdecimal'
+require 'bigdecimal/util'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, :region, :dob, :gender, :language, presence: true
+  validates :username, :region, :dob, :gender, presence: true
   validates :username, format: { with: /\A[A-Za-z0-9\-_.]+\z/i,
                                  message: "Please use only letters from the latin alphabet,
                                            numbers, and punctuation: -_." },
                        length: { minimum: 3, maximum: 255 },
                        uniqueness: true
-  validates :region, inclusion: { in: @regions,
+  validates :region, inclusion: { in: ["Europe",
+                                       "North America",
+                                       "South America",
+                                       "Africa",
+                                       "Middle East",
+                                       "East Asia",
+                                       "Southeast Asia",
+                                       "Australia & Pacific",
+                                       "Antarctica"
+                                      ],
                                   message: "%<value> is not a valid region" }
 
   # validation for languages included in variable below
   validate :validate_age
-  validate :gender, inclusion: { in: ["Male", "Female", "Non-binary", "Prefer not to say"] }
+  validates :gender, inclusion: { in: ["Male", "Female", "Other", "Prefer not to say"] }
 
   # validation for favourite users
   # validation for avoid users
@@ -28,22 +40,10 @@ class User < ApplicationRecord
   private
 
   def validate_age
-    if dob.present? && dob > 18.years.ago.to_d
+    if dob.present? && dob > 18.years.ago
       errors.add(:dob, 'You must be at least 18 years old.')
     end
   end
-
-  @regions = [
-    "Europe",
-    "North America",
-    "South America",
-    "Africa",
-    "Middle East",
-    "East Asia",
-    "Southeast Asia",
-    "Australia & Pacific",
-    "Antarctica"
-  ]
 
   @languages = %w[
     English
