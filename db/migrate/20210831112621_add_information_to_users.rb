d7f11b7483ead391d46ed977c398b78a1aa992ec
Louis Drinkwater <izumilouis95@gmail.com>
2021-08-31 14:58:40 +0100

    Remove unnecessary columns from user migration file

--- db/migrate/20210831112621_add_information_to_users.rb
+++ db/migrate/20210831112621_add_information_to_users.rb
@@ -1,8 +1,6 @@
 class AddInformationToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :email, :string
     add_column :users, :username, :string
-    add_column :users, :password, :string
     add_column :users, :region, :string
     add_column :users, :dob, :date
     add_column :users, :gender, :string
