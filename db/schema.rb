1269a6ce1da411f99d7db152c5011a4c85a26456
Jacob Chew <jacobjcchew@gmail.com>
2021-08-31 15:43:25 +0100

    Fixed conflicts, and migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_110120) do
+ActiveRecord::Schema.define(version: 2021_08_31_120536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.boolean "ongoing"
+    t.string "status"
+    t.integer "users", default: [], array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "user_ratings", force: :cascade do |t|
     t.float "rating"
@@ -23,13 +59,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_110120) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_user_ratings_on_user_id"
-  create_table "sessions", force: :cascade do |t|
-    t.integer "game_id"
-    t.boolean "ongoing"
-    t.string "status"
-    t.text "users", default: [], array: true
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,9 +69,29 @@ ActiveRecord::Schema.define(version: 2021_08_31_110120) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "region"
+    t.date "dob"
+    t.string "gender"
+    t.string "language", default: [], array: true
+    t.string "steam_handle"
+    t.string "origin_handle"
+    t.string "psn_handle"
+    t.string "epic_handle"
+    t.string "discord_handle"
+    t.string "live_handle"
+    t.string "gog_handle"
+    t.string "twich_handle"
+    t.string "battlenet_handle"
+    t.string "favourite_games", default: [], array: true
+    t.integer "favourite_users", default: [], array: true
+    t.integer "avoid_users", default: [], array: true
+    t.integer "user_status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "user_ratings", "users"
 end
