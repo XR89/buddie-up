b70a6d69f15e0030774329808e2024b6e1d86659
Jacob Chew <jacobjcchew@gmail.com>
2021-09-01 17:29:42 +0100

    Updated game sessions controller, fixed bugs, added migration file and started with show page

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -2,5 +2,6 @@ class GameSessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
   def show
     @gamesession = GameSession.find(params[:id])
+    @message = Message.new
   end
 end
