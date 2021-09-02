69b50517f3cef878309290f01be6b48c6237ff2a
Xander Reardon <xanderreardon@gmail.com>
2021-09-02 11:39:59 +0100

    commit before master branch

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    width: '300px',
+    dropdownCssClass: 'test'
+  });
 };
 
 export { initSelect2 };
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#profile', as: 'profile'
   resources :users, only: %i[index show]
   resources :games, only: %i[index show]
-<<<<<<< HEAD
-  resources :game_session, only: %i[show] do
-=======
   resources :game_sessions, only: %i[show] do
->>>>>>> master
     resources :user_ratings, only: %i[new create], as: :ratings
     resources :messages, only: :create
   end
