a497436d488259c17a011502e6b031482571f0a1
Xander Reardon <xanderreardon@gmail.com>
2021-09-06 17:30:07 +0100

    commit before master merge/heroku push

--- app/controllers/avoid_users_controller.rb
+++ app/controllers/avoid_users_controller.rb
@@ -6,6 +6,9 @@ class AvoidUsersController < ApplicationController
     @avoid_user.save!
   if @avoid_user.save
     redirect_back(fallback_location: root_path)
+
+
+    # redirect above to home/profile so they dont have to see tht dickhead again
   end
 
   end
