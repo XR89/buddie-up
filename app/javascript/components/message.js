1fba5a9c9190c46dae5c321e7858fbd8ce069fe3
Jacob Chew <jacobjcchew@gmail.com>
2021-09-08 17:29:16 +0100

    Halfway

--- app/javascript/components/message.js
+++ app/javascript/components/message.js
@@ -1,19 +1,17 @@
 const message = () => {
   const form = document.querySelector('form');
-  document.getElementById('message_content').focus();
-  document.getElementById('message_content').select();
+  console.log(form);
+  // document.getElementById('message_content').mouseup();
+  console.log(document.getElementById('message_content').focus());
+  console.log(document.getElementById('message_content').select());
   document.addEventListener('keydown', (e) => {
     const usermessage = document.getElementById('message_content').value
+    console.log(usermessage);
     if (e.code == 'Enter' && usermessage.length != 0 ) {
       form.submit();
     }
   })
 }
 
-// const usermessage = document.getElementById('usermessage');
-
-// const submitmessage = (e) =>  {
-//   e.insertAdjacentHTML("beforeend", usermessage.textarea.value)
-// }
 
 export { message };
