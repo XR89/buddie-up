8faef8c9d37222f8170c5a977eeca12ac37edd25
Jacob Chew <jacobjcchew@gmail.com>
2021-09-08 17:38:45 +0100

    Fixed javascript

--- app/javascript/components/message.js
+++ app/javascript/components/message.js
@@ -2,8 +2,11 @@ const message = () => {
   const form = document.querySelector('form');
   console.log(form);
   // document.getElementById('message_content').mouseup();
-  console.log(document.getElementById('message_content').focus());
-  console.log(document.getElementById('message_content').select());
+  console.log(document.querySelector('.chat--input').focus());
+  console.log(document.querySelector('.chat--input').select());
+
+  // console.log(document.getElementById('message_content').focus());
+  // console.log(document.getElementById('message_content').select());
   document.addEventListener('keydown', (e) => {
     const usermessage = document.getElementById('message_content').value
     console.log(usermessage);
