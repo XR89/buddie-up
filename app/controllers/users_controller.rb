9fb98c2d72a8fa747a66e4bc961a2697db31a1f1
Xander Reardon <xanderreardon@gmail.com>
2021-09-09 10:12:17 +0100

    attempted avoid logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where.not(id: @avoided_users_array)
+    @users = User.where.not(id: @current_user_avoided_users_array)
     @favourite_users = current_user.favourite_users
     @favourite_user_ids = @favourite_users.map { |user| user.id}
 
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
     @invitations = @user.invitations unless @user.invitations.nil?
     @favourite_games = users_favourite_games(@user.id)
     redirect_to profile_path if current_user == @user
-    redirect_to profile_path if @avoided_users_array.include?(@user.id)
+    redirect_to profile_path if @current_user_avoided_users_array.include?(@user.id)
+    redirect_to profile_path if @user.avoid_user_ids.include?(current_user.id)
     @favourite_user = FavouriteUser.find_by(favourite_user: params[:id], user: current_user)
     @avoided_user = AvoidUser.find_by(avoid_user: params[:id], user: current_user)
   end
@@ -37,12 +38,11 @@ class UsersController < ApplicationController
   private
 
   def find_avoided_users
-    @avoided_users_array = current_user.avoid_users.map do |x|
+    @current_user_avoided_users_array = current_user.avoid_users.map do |x|
       x.avoid_user_id
     end
-  end
 
-  private
+  end
 
   def users_favourite_games(user_id)
     favourited_array = FavouriteGame.where(user_id: user_id)
