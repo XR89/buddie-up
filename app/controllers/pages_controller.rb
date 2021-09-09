75c581fb5b68b2f5e7585ff1bdaa7c310cd1ce46
Jacob Chew <jacobjcchew@gmail.com>
2021-09-09 13:49:16 +0100

    Added flash messages and javascript

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,11 @@ class PagesController < ApplicationController
   def home
     # newsfeed for logged in user
     @user = current_user
-    redirect_to welcome_path, alert: 'Welcome! Please signup or login before continuing' unless current_user
+    redirect_to welcome_path unless current_user
   end
 
   def welcome
     # banner with call-to-action
-    redirect_to root_path, notice: 'Welcome back!' if current_user
+    redirect_to root_path if current_user
   end
 end
