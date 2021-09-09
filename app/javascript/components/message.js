4c6cf652e910257bb7593088a172300ed28cc7e0
Jacob Chew <jacobjcchew@gmail.com>
2021-09-09 10:53:15 +0100

    Added flash messages and javascript to make it fade

--- app/javascript/components/message.js
+++ app/javascript/components/message.js
@@ -2,9 +2,9 @@ const message = () => {
   const form = document.querySelector('form');
   // document.getElementById('message_content').mouseup();
   setTimeout(() => {
-    document.querySelector('message_content').focus();
+    document.querySelector('#message_content').focus();
   }, 300);
-  document.querySelector('message_content').select();
+  document.querySelector('#message_content').select();
   // console.log(document.getElementById('message_content').focus());
   // console.log(document.getElementById('message_content').select());
   document.addEventListener('keydown', (e) => {
